748be45959241d10e5d9678a413d5ab38c9b27a4
João Castro <joaofbomfimc@gmail.com>
2023-03-12 19:27:51 -0300

    :sparkles: Fim da aula

--- recipes/views.py
+++ recipes/views.py
@@ -1,26 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404, get_object_or_404
 from .models import Recipe
 
 
 def home(request):
-    recipes = Recipe.objects.filter(is_published=True).order_by('-id')
+    recipes = get_list_or_404(
+        Recipe.objects.filter(
+            is_published=True
+        ).order_by('-id')
+    )
+
     return render(request, 'recipes/pages/home.html', context={
         'recipes': recipes,
     })
 
 
 def category(request, category_id: int):
-    recipes = Recipe.objects.filter(
-        category__id=category_id,
-        is_published=True
-    ).order_by('-id')
+    recipes = get_list_or_404(
+        Recipe.objects.filter(
+            category__id=category_id,
+            is_published=True
+        ).order_by('-id')
+    )
+
     return render(request, 'recipes/pages/categories.html', context={
         'recipes': recipes,
+        'title': f'{recipes[0].category.name} - Category | '
     })
 
 
 def recipe(request, id: int):
-    recipe = Recipe.objects.get(id=id)
+    recipe = get_object_or_404(Recipe, id=id, is_published=True)
+
     return render(request, 'recipes/pages/recipe-view.html', context={
         'recipe': recipe,
         'is_detail_page': True
