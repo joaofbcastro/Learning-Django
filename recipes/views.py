f2a0a3265a5fb016737e553a4f02e934101e9339
João Castro <joaofbomfimc@gmail.com>
2023-03-12 16:34:06 -0300

    :sparkles: Adicionando coisas na tela que estão no banco de dados

--- recipes/views.py
+++ recipes/views.py
@@ -1,48 +1,27 @@
-from random import randint
-from faker import Faker
 from django.shortcuts import render
-# Create your views here.
+from .models import Recipe
 
 
 def home(request):
+    recipes = Recipe.objects.filter(is_published=True).order_by('-id')
     return render(request, 'recipes/pages/home.html', context={
-        'recipes': [make_recipe() for _ in range(6)],
+        'recipes': recipes,
+    })
+
+
+def category(request, category_id: int):
+    recipes = Recipe.objects.filter(
+        category__id=category_id,
+        is_published=True
+    ).order_by('-id')
+    return render(request, 'recipes/pages/categories.html', context={
+        'recipes': recipes,
     })
 
 
 def recipe(request, id: int):
+    recipe = Recipe.objects.get(id=id)
     return render(request, 'recipes/pages/recipe-view.html', context={
-        'recipe': make_recipe(),
+        'recipe': recipe,
         'is_detail_page': True
     })
-
-
-def rand_ratio():
-    return randint(800, 900), randint(300, 500)
-
-
-fake = Faker('pt_BR')
-
-
-def make_recipe():
-    return {
-        'id': fake.random_number(digits=6, fix_len=True),
-        'title': fake.sentence(nb_words=6),
-        'description': fake.sentence(nb_words=12),
-        'preparation_time': fake.random_number(digits=2, fix_len=True),
-        'preparation_time_unit': 'Minutos',
-        'servings': fake.random_number(digits=2, fix_len=True),
-        'servings_unit': 'Porção',
-        'preparation_steps': fake.text(3000),
-        'created_at': fake.date_time(),
-        'author': {
-            'first_name': fake.first_name(),
-            'last_name': fake.last_name(),
-        },
-        'category': {
-            'name': fake.word()
-        },
-        'cover': {
-            'url': 'https://loremflickr.com/%s/%s/food,cook' % rand_ratio(),
-        }
-    }
