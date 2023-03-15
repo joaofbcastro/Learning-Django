2ed58a32851084d3d05761c70f1c154e6b5ce5be
João Castro <joaofbomfimc@gmail.com>
2023-03-15 11:54:51 -0300

    :test_tube: Adiciona os testes de Views

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
-from django.urls import reverse
+from django.urls import reverse, resolve
+from recipes import views
 
 
 class RecipesURLsTest(TestCase):
@@ -14,3 +15,17 @@ class RecipesURLsTest(TestCase):
     def test_recipes_recipe_url_is_correct(self):
         url = reverse('recipes:recipe', args=(1,))
         self.assertEqual(url, '/recipes/1/')
+
+
+class RecipesViewsTest(TestCase):
+    def test_recipes_home_view_is_correct(self):
+        view = resolve(reverse('recipes:home'))
+        self.assertIs(view.func, views.home)
+
+    def test_recipes_category_view_is_correct(self):
+        view = resolve(reverse('recipes:category', kwargs={'category_id': 1}))
+        self.assertIs(view.func, views.category)
+
+    def test_recipes_recipe_view_is_correct(self):
+        view = resolve(reverse('recipes:recipe', args=(1,)))
+        self.assertIs(view.func, views.recipe)
